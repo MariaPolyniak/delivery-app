1ed0eb9be6842e191faca6d59e2ed5656b234c41
MariaPolyniak <masha.poliniak@gmail.com>
2021-03-18 00:15:55 +0200

    add initial settings

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const path = require("path");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
