c5469827476410b8d23a3b0e1e3552c6b823bfed
MariaPolyniak <masha.poliniak@gmail.com>
2021-03-18 15:26:55 +0200

    retrieve data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from './Container';
 import Header from './Header';
 import MenuList from './MenuList';
@@ -6,6 +6,22 @@ import SideBar from './SideBar';
 import Footer from './Footer';
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.jsonbin.io/b/605347b3683e7e079c53b0cc', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'secret-key':
+          '$2b$10$Z/bNYzk9LegF44CW8Q1JMOSeBeZuxgFpINTLWRGmZwE.YKxHWKpmW',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <Container>
       <Header />
